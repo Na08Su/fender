f90676d56ef4f772ca27f2a797ecc5773d30669a
NatsukiSugawara <snsn19910803@gmail.com>
2018-09-13 17:57:32 +0900

    サーバー

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,13 +1,17 @@
 set :stage, :production
 set :branch, 'master' ## 必要があれば変更
 
-role :app, %w{root@118.27.9.161 } ###変更
-role :web, %w{root@118.27.9.161 } ###
-role :db, %w{root@118.27.9.161 }  ###
+# role :app, %w{nattu@118.27.9.161 }
+# role :web, %w{nattu@118.27.9.161 }
+# role :db, %w{nattu@118.27.9.161 }
 
-set :ssh_options, {
-  port: 6183, #### 変更
-  keys: [File.expand_path('~/.ssh/github/id_rsa')],
-  forward_agent: true,
-  auth_methods: %w(publickey)
-}
+server '118.27.9.161', user: 'nattu', roles: %w{app db web}, port: 6183
+
+#デプロイするサーバーにsshログインする鍵の情報。サーバー編で作成した鍵のパス
+set :ssh_options, keys: '~/.ssh/github/id_rsa'
+# set :ssh_options, {
+#   port: 6183,
+#   keys: [File.expand_path('~/.ssh/github/id_rsa')],
+#   forward_agent: true,
+#   auth_methods: %w(publickey)
+# }
