ef2ea826fca0b050ad0140f1a52dc45aa34fb16b
NatsukiSugawara <snsn19910803@gmail.com>
2018-09-13 17:51:24 +0900

    ssh_optionの修正

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,13 +1,13 @@
 set :stage, :production
 set :branch, 'master' ## 必要があれば変更
 
-role :app, %w{nattu@118.27.9.161 } ###変更
-role :web, %w{nattu@118.27.9.161 } ###
-role :db, %w{nattu@118.27.9.161 }  ###
+role :app, %w{root@118.27.9.161 } ###変更
+role :web, %w{root@118.27.9.161 } ###
+role :db, %w{root@118.27.9.161 }  ###
 
 set :ssh_options, {
   port: 6183, #### 変更
-  keys: [File.expand_path('~/.ssh/insecure_private_key')],
+  keys: [File.expand_path('~/.ssh/github/id_rsa')],
   forward_agent: true,
   auth_methods: %w(publickey)
 }
