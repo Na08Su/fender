5ef26f9ba0304d2e6b0bcf18af77a38159a06a2e
NatsukiSugawara <snsn19910803@gmail.com>
2018-09-14 12:51:34 +0900

    ssh-agent

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,17 +1,14 @@
 set :stage, :production
 set :branch, 'master' ## 必要があれば変更
 
-# role :app, %w{nattu@118.27.9.161 }
-# role :web, %w{nattu@118.27.9.161 }
-# role :db, %w{nattu@118.27.9.161 }
-
 server '118.27.9.161', user: 'nattu', roles: %w{app db web}, port: 6183
 
-#デプロイするサーバーにsshログインする鍵の情報。サーバー編で作成した鍵のパス
-set :ssh_options, keys: '~/.ssh/fender/meloon'
-# set :ssh_options, {
-#   port: 6183,
-#   keys: [File.expand_path('~/.ssh/github/id_rsa')],
-#   forward_agent: true,
-#   auth_methods: %w(publickey)
-# }
+#デプロイするサーバーにsshログインする鍵の情報。
+#サーバー編で作成した鍵のパス
+set :ssh_options, {
+  port: 6183,
+  # capistranoコマンド実行者の秘密鍵
+  keys: [File.expand_path('~/.ssh/fender/meloon')],
+  forward_agent: true,
+  auth_methods: %w(publickey)
+}
