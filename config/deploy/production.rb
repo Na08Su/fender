8a04779ad8bac2c8cfa0be11df302305bbe8f1d0
NatsukiSugawara <snsn19910803@gmail.com>
2018-09-14 13:00:27 +0900

    修正

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -8,7 +8,7 @@ server '118.27.9.161', user: 'nattu', roles: %w{app db web}, port: 6183
 set :ssh_options, {
   port: 6183,
   # capistranoコマンド実行者の秘密鍵
-  keys: [File.expand_path('~/.ssh/fender/meloon')],
+  keys: %w('~/.ssh/fender/meloon'),
   forward_agent: true,
   auth_methods: %w(publickey)
 }
