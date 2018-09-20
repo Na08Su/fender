1a75665cded984ee15593db13d98d9c8b0ba49ed
NatsukiSugawara <snsn19910803@gmail.com>
2018-09-20 16:14:16 +0900

    default_env設定

--- config/deploy.rb
+++ config/deploy.rb
@@ -17,7 +17,7 @@ set :pty, true # sudoを使用するのに必要
 set :bundle_binstubs, nil
 
 # capistrano用でbundleするのに必要
-set :default_env, { path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH"} # 
+set :default_env, { path: "/home/nattu/.rbenv"}
 
 
 # Shared に入るものを指定
