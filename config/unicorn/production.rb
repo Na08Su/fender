b59a8b95c77f15c741d6cfd6cef528dfbdd723ed
NatsukiSugawara <snsn19910803@gmail.com>
2018-09-21 19:07:27 +0900

    kill スキップ

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -37,7 +37,7 @@ puts "-----------------------------------------------------------------"
 before_fork do |server, worker|
   Signal.trap 'TERM' do
     puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
-    Process.kill 'QUIT', Process.pid
+    # Process.kill 'QUIT', Process.pid
   end
 
   defined?(ActiveRecord::Base) and
