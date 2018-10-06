4220e2b8b8838e16a048d5cf0004c612b3b09e87
NatsukiSugawara <snsn19910803@gmail.com>
2018-10-06 19:30:59 +0900

    seed編集

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,7 +10,11 @@ AdminUser.create!(email: 'admin@example.com', password: 'password', password_con
 # production => コマンドから直接登録することも検討
 AdminUser.create!(email: 'koki0601@example.com', password: 'koki0601+', password_confirmation: 'koki0601+') if Rails.env.production?
 AdminUser.create!(email: 'nattu0803@example.com', password: 'nattu0803+', password_confirmation: 'nattu0803+') if Rails.env.production?
+AdminUser.create!(email: 'mikami0303@example.com', password: 'mikami0303+', password_confirmation: 'mikami0303+') if Rails.env.production?
+AdminUser.create!(email: 'ryota0726@example.com', password: 'ryota0726+', password_confirmation: 'ryota0726+') if Rails.env.production?
+
 AdminUser.create!(email: 'external@example.com', password: 'exterminator', password_confirmation: 'exterminator') if Rails.env.production?
+
 
 categories = ["荒野行動", "ポケモン", "ドラクエ", "ウイイレ", "パワプロ", "FF", "桃鉄", "NIKE", "Puma", "Adidas"]
 
